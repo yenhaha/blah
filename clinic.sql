02699fa35929d53f38a10c94ac84b13d39360688
yenhaha <hulyenhaha@gmail.com>
2015-03-06 04:10:02 +0800

    added clinic.sql

--- clinic.sql
+++ clinic.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 05, 2015 at 09:04 PM
+-- Generation Time: Mar 05, 2015 at 09:09 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
